f20f2165ab0b1c7caed87a1863a3c71914107a85
Hubert51 <ruijiegeng@gmail.com>
2017-11-04 18:13:20 -0400

    Add input files into test folder. Arrange file in test folder

--- Test_code/test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-image = cv2.imread('ironman.jpg')
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('gray_image.png',gray_image)
-cv2.imshow('color_image',image)
-cv2.imshow('gray_image',gray_image) 
-cv2.waitKey(0)                 # Waits forever for user to press any key
-cv2.destroyAllWin
